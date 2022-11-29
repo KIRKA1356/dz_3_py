83386edbcd239da7e4fd6da5c25a5e97fca42e4c
KIRKA1356 <kgritsun@mail.ru>
2022-11-29 14:10:18 +0300

    Over

--- dz_3.py
+++ dz_3.py
@@ -22,10 +22,31 @@
 
 # Задача 2(3.11)
 
-def pruf():
-    """
-    Проверка дроби
-    """
-    num = (input("Введите ваше число: "))
-    prof = num.find("/" or ".")
-    print(prof)
+# def pruf():
+#     """
+#     Проверка дроби
+#     """
+#     num = (input("Введите ваше число: "))
+#     prof = num.find(".")
+#     prof2 = num.find(",")
+#     if prof>0 or prof2>0: print("Ваше число дробное")
+#     else: print("Ваше число не является дробью")
+# pruf()
+
+# Задача 3(3.12)
+
+# def cut():
+#     text = (input("Введите ваш текст и нажмите ENTER: "))
+#     index = text.find('о')
+#     index2 = text.rfind('о', text.find('о')-1 )  
+    
+#     if index<0 and index2<0: print("В вашем тексте нет буквы о!")
+    
+#     elif index>0 and index2==index: 
+#         print("В вашем тексте одна буква о, вот текст после нее и до конца текста:")
+#         print(text[index+1:])
+#     else: 
+#         print("Вот ваш срез: ")
+#         print(text[index+1:index2])
+    
+# cut()
